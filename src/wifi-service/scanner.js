4d540b3972c03fef62edbb655f86b967de47cf8f
João Sombrio <chrisparkerrr@gmail.com>
2019-08-11 05:00:43 -0300

    Simplified scanner and fixed a bug

--- src/wifi-service/scanner.js
+++ src/wifi-service/scanner.js
@@ -1,26 +1,23 @@
-import { init, scan as _scan } from 'node-wifi';
+import { init, scan as _scan } from 'node-wifi'
 
 const scan = () => {
   init({
-    iface: null
-  });
+    iface: null,
+  })
 
-  _scan(function(error, networks) {
-    let scanPromise = 
-      _scan()
-        .then((networks) => {
-          networks.map(network => ({
-            ssid: network.ssid,
-            mac: network.mac,
-            channel: network.channel,
-            frequency: network.frequency,
-            signal: network.quality,
-            security: network.security
-          }))
-        })
-        .catch((error) => { error });
-    return scanPromise;
-  });
+  const scanPromise = _scan()
+    .then((networks) => {
+      networks.map((network) => ({
+        ssid: network.ssid,
+        mac: network.mac,
+        channel: network.channel,
+        frequency: network.frequency,
+        signal: network.quality,
+        security: network.security,
+      }))
+    })
+    .catch((error) => error)
+  return scanPromise
 }
 
 export default scan
